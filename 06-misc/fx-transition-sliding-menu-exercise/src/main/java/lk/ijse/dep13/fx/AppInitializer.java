7a1096b6d524c880095a90b4bfce60afa0f09fa0
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-21 12:29:52 +0530

    Finish the exercise

--- 06-misc/fx-transition-sliding-menu-exercise/src/main/java/lk/ijse/dep13/fx/AppInitializer.java
+++ 06-misc/fx-transition-sliding-menu-exercise/src/main/java/lk/ijse/dep13/fx/AppInitializer.java
@@ -3,6 +3,7 @@ package lk.ijse.dep13.fx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +16,11 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/MainScene.fxml"))));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/scene/MainScene.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        root.setFocusTraversable(true); // You can enable/disable this via SceneBuilder also
+        root.requestFocus();
         primaryStage.setTitle("FX Transition: Sliding Menu Exercise");
         primaryStage.show();
         primaryStage.centerOnScreen();
--- 06-misc/fx-transition-sliding-menu-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 06-misc/fx-transition-sliding-menu-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -3,8 +3,11 @@ package lk.ijse.dep13.fx.controller;
 import javafx.animation.TranslateTransition;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class MainSceneController {
@@ -17,31 +20,42 @@ public class MainSceneController {
     public AnchorPane root;
 
     public void initialize() {
-        mainMenu.setTranslateX(-mainMenu.getPrefWidth());
+        mainMenu.setTranslateX(-mainMenu.getPrefWidth() - 2);
     }
 
     public void imgMenuOnMouseClicked(MouseEvent event) {
         TranslateTransition tt = new TranslateTransition(Duration.millis(500), mainMenu);
-        tt.setFromX(-mainMenu.getPrefWidth());
+        tt.setFromX(-mainMenu.getPrefWidth() - 2);
         tt.setToX(0);
         tt.playFromStart();
         event.consume();
     }
 
     public void rootOnMouseClicked(MouseEvent event) {
+        hideMenu();
+    }
+
+    public void rootOnKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ESCAPE) hideMenu();
+    }
+
+    private void hideMenu(){
+        if (mainMenu.getTranslateX() != 0) return;
         TranslateTransition tt = new TranslateTransition(Duration.millis(500), mainMenu);
         tt.setFromX(0);
-        tt.setToX(-mainMenu.getPrefWidth());
+        tt.setToX(-mainMenu.getPrefWidth() - 2);
         tt.playFromStart();
     }
 
     public void lblMenuOnMouseEntered(MouseEvent mouseEvent) {
         Label lblMenu = (Label) mouseEvent.getTarget();
-        lblMenu.setStyle("-fx-text-fill: red;");
+        lblMenu.setTextFill(Color.AQUA);
     }
 
     public void lblMenuOnMouseExited(MouseEvent mouseEvent) {
         Label lblMenu = (Label) mouseEvent.getTarget();
-        lblMenu.setStyle("-fx-text-fill: white;");
+        lblMenu.setTextFill(Color.WHITE);
     }
+
+
 }

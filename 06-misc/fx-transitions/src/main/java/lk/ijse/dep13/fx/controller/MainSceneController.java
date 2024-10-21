22a0828b123464f5bb7b45b4cb2f9d71af5c069a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-21 10:29:38 +0530

    Update the MainSceneController

--- 06-misc/fx-transitions/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 06-misc/fx-transitions/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -1,4 +1,41 @@
 package lk.ijse.dep13.fx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MainSceneController {
+
+    public Button btnReset;
+    public Button btnRotate;
+    public Button btnScaleX;
+    public Button btnScaleY;
+    public Button btnTranslateX;
+    public Button btnTranslateY;
+    public Label lblPreview;
+
+    public void btnResetOnAction(ActionEvent event) {
+
+    }
+
+    public void btnRotateOnAction(ActionEvent event) {
+
+    }
+
+    public void btnScaleXOnAction(ActionEvent event) {
+
+    }
+
+    public void btnScaleYOnAction(ActionEvent event) {
+
+    }
+
+    public void btnTranslateXOnAction(ActionEvent event) {
+
+    }
+
+    public void btnTranslateYOnAction(ActionEvent event) {
+
+    }
+
 }

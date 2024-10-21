a518ba0fe2b0bc68c26f9a40b5a51eda190a1590
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-21 10:59:28 +0530

    Add few transitions

--- 06-misc/fx-transitions/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 06-misc/fx-transitions/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -1,8 +1,11 @@
 package lk.ijse.dep13.fx.controller;
 
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class MainSceneController {
 
@@ -15,27 +18,46 @@ public class MainSceneController {
     public Label lblPreview;
 
     public void btnResetOnAction(ActionEvent event) {
+        lblPreview.setTranslateX(0);
+        lblPreview.setTranslateY(0);
+        lblPreview.setScaleX(0);
+        lblPreview.setScaleY(0);
+    }
 
+    public void btnScaleXOnAction(ActionEvent event) {
+        ScaleTransition st = new ScaleTransition(Duration.millis(500), lblPreview);
+        st.setFromX(1);
+        st.setToX(1.5);
+        st.playFromStart();
+    }
+
+    public void btnScaleYOnAction(ActionEvent event) {
+        ScaleTransition st = new ScaleTransition(Duration.millis(500), lblPreview);
+        st.setFromY(1);
+        st.setToY(1.5);
+        st.playFromStart();
+    }
+
+    public void btnTranslateXOnAction(ActionEvent event) {
+        TranslateTransition tt = new TranslateTransition(Duration.millis(500), lblPreview);
+        tt.setFromX(-100);
+        tt.setToX(0);
+        tt.playFromStart();
+        //lblPreview.setTranslateX(100);
+    }
+
+    public void btnTranslateYOnAction(ActionEvent event) {
+        TranslateTransition tt = new TranslateTransition(Duration.millis(500), lblPreview);
+        tt.setFromY(100);
+        tt.setToY(0);
+        tt.playFromStart();
+        //lblPreview.setTranslateY(100);
     }
 
     public void btnRotateOnAction(ActionEvent event) {
 
     }
 
-    public void btnScaleXOnAction(ActionEvent event) {
 
-    }
-
-    public void btnScaleYOnAction(ActionEvent event) {
-
-    }
-
-    public void btnTranslateXOnAction(ActionEvent event) {
-
-    }
-
-    public void btnTranslateYOnAction(ActionEvent event) {
-
-    }
 
 }

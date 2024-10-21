9e8e4cb15f17ef245a8dc2ded95d045d9344713e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-21 15:02:20 +0530

    Add walking images

--- 06-misc/fx-animations-game-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 06-misc/fx-animations-game-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -16,6 +16,8 @@ public class MainSceneController {
     public ImageView character;
     int t = 0;
     int imageIndex = 1;
+    int dx = 0;
+
     public void initialize() throws InterruptedException {
 //        while (character.getLayoutY() < root.getPrefHeight() - ground.getHeight() - character.getHeight()){
 //            character.setLayoutY(character.getLayoutY() + 10);
@@ -23,16 +25,22 @@ public class MainSceneController {
 //        }
         //int dy = 10;
         final double gravity = 7;
-        var keyFrame1 = new KeyFrame(Duration.millis(1000 / 27.), (e)->{
+        var keyFrame1 = new KeyFrame(Duration.millis(1000 / 27.), (e) -> {
             double dy = gravity * t++;
             if (((character.getLayoutY() + dy) < root.getPrefHeight() - ground.getHeight() - character.getFitHeight())) {
                 character.setLayoutY(character.getLayoutY() + dy);
-            }else{
+            } else {
                 t = 0;
                 character.setLayoutY(root.getPrefHeight() - ground.getHeight() - character.getFitHeight());
             }
-            character.setImage(new Image("/image/idle/Idle (%d).png".formatted(imageIndex++)));
-            if (imageIndex > 10) imageIndex = 1;
+            if (dx != 0) {
+                character.setLayoutX(character.getLayoutX() + dx);
+//                character.setImage(new Image("/image/run/Run (%d).png".formatted(imageIndex++)));
+//                if (imageIndex > 8) imageIndex = 1;
+            } else {
+                character.setImage(new Image("/image/idle/Idle (%d).png".formatted(imageIndex++)));
+                if (imageIndex > 10) imageIndex = 1;
+            }
         });
         Timeline tl = new Timeline(keyFrame1);
         tl.setDelay(Duration.millis(500));
@@ -41,12 +49,21 @@ public class MainSceneController {
     }
 
     public void rootOnKeyPressed(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.SPACE){
+        if (keyEvent.getCode() == KeyCode.SPACE) {
             character.setLayoutY(character.getLayoutY() - 175);
-        }else if (keyEvent.getCode() == KeyCode.LEFT){
-            character.setLayoutX(character.getLayoutX() - 10);
+        } else if (keyEvent.getCode() == KeyCode.LEFT) {
+            //if (dx == 0) imageIndex = 1;
+            dx = -10;
         } else if (keyEvent.getCode() == KeyCode.RIGHT) {
-            character.setLayoutX(character.getLayoutX() + 10);
+            //if (dx == 0) imageIndex = 1;
+            dx = 10;
+        }
+    }
+
+    public void rootOnKeyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.LEFT || keyEvent.getCode() == KeyCode.RIGHT) {
+            //imageIndex = 1;
+            dx = 0;
         }
     }
 }

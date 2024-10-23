3611566df57876cb7a7d89a29b3d49ed8135aa6f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-23 12:02:22 +0530

    Finish the fx scene navigation exercise

--- 04-navigation/fx-scene-navigation-exercise-1/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 04-navigation/fx-scene-navigation-exercise-1/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -3,11 +3,16 @@ package lk.ijse.dep13.fx.controller;
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class MainSceneController {
 
@@ -17,6 +22,13 @@ public class MainSceneController {
     public ImageView imgStock;
     public Label lblDescription;
     public AnchorPane root;
+
+    public void initialize(){
+        FadeTransition ft = new FadeTransition(Duration.millis(500), root);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.playFromStart();
+    }
 
     public void imgOnMouseEntered(MouseEvent event) {
         ScaleTransition ft = new ScaleTransition(Duration.millis(200), (ImageView) event.getTarget());
@@ -47,5 +59,21 @@ public class MainSceneController {
         ft.playFromStart();
 
         lblDescription.setText("Select a menu to proceed");
+    }
+
+    public void imgOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        ImageView menuItem = (ImageView) mouseEvent.getTarget();
+        if (menuItem == imgCustomer){
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/ManageCustomerScene.fxml"))));
+        }else if (menuItem == imgOrder){
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/PlaceOrderScene.fxml"))));
+        }else if (menuItem == imgStock){
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/ManageStockScene.fxml"))));
+        }else{
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/ConfigureSettingsScene.fxml"))));
+        }
+        stage.sizeToScene();
+        stage.centerOnScreen();
     }
 }
--- 04-navigation/fx-scene-navigation-exercise-1/src/main/java/lk/ijse/dep13/fx/controller/ManageCustomerSceneController.java
+++ 04-navigation/fx-scene-navigation-exercise-1/src/main/java/lk/ijse/dep13/fx/controller/ManageCustomerSceneController.java
@@ -1,13 +1,31 @@
 package lk.ijse.dep13.fx.controller;
 
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class ManageCustomerSceneController {
     public AnchorPane root;
     public Button btnBack;
 
-    public void btnBackOnAction(ActionEvent actionEvent) {
+    public void initialize(){
+        FadeTransition ft = new FadeTransition(Duration.millis(500), root);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.playFromStart();
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/MainScene.fxml"))));
+        stage.sizeToScene();
+        stage.centerOnScreen();
     }
 }

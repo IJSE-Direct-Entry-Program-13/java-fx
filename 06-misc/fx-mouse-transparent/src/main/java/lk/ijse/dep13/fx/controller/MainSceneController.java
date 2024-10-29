4808aec2d4788d4629f9c50ef89a2d6f56328170
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-29 15:52:23 +0530

    Add Mouse Transparent Project

---
+++ 06-misc/fx-mouse-transparent/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep13.fx.controller;
+
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class MainSceneController {
+    public AnchorPane container;
+
+    public void containerOnMouseClicked(MouseEvent mouseEvent) {
+    }
+}

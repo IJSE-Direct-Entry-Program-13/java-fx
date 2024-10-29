c3d872e8df5a3a17fc97ca645e23147b83a14e7c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-29 15:38:42 +0530

    Add fx clipboard handling project

---
+++ 06-misc/fx-clipboard-handling/src/main/java/lk/ijse/dep13/fx/AppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep13.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/scene/MainScene.fxml"))));
+        primaryStage.setTitle("FX Clipboard");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
---
+++ 06-misc/fx-clipboard-handling/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep13.fx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainSceneController {
+    public Button btnReadText;
+    public Button btnReadFiles;
+    public Button btnSetText;
+    public Button btnSetFiles;
+    public Button btnClear;
+
+    public void btnSetTextOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSetFilesOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnReadFilesOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnReadTextOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+
+    }
+}

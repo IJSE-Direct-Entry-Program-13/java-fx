8c763458a47d48d59e6be87ee393da8d1de1163d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-29 15:15:43 +0530

    Consum startDragAndDrop

--- 05-controls/fx-controls-drag-and-drop-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 05-controls/fx-controls-drag-and-drop-exercise/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -1,4 +1,17 @@
 package lk.ijse.dep13.fx.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.*;
+
 public class MainSceneController {
+    public void lblOnDragDetected(MouseEvent mouseEvent) {
+        Label lbl = (Label) mouseEvent.getSource();
+        Dragboard dragboard = lbl.startDragAndDrop(TransferMode.MOVE);
+        ClipboardContent clipboardContent = new ClipboardContent();
+        clipboardContent.putString(lbl.getStyle());
+        dragboard.setDragView(lbl.snapshot(null, null));
+        dragboard.setContent(clipboardContent);
+        mouseEvent.consume();
+    }
+
 }

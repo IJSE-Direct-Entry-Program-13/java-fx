e9471772f24735bd578a56d49274504522cbd396
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-30 17:48:16 +0530

    Finish controls api

--- 05-controls/fx-controls-4/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 05-controls/fx-controls-4/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -7,6 +7,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
+import java.time.LocalDate;
+
 public class MainSceneController {
     public Slider sldrProgress;
     public ProgressBar prgBar;
@@ -20,8 +22,66 @@ public class MainSceneController {
     public Button btnShowHide;
     public TextField txtPlainPassword;
     public PasswordField txtPassword;
+    public DatePicker dtPicker;
+    public Label lblPreview;
+    public ToggleButton tglBold;
+    public ToggleButton tglItalic;
+    public Label lblPreviewText;
+    public CheckBox chkItalic;
+    public CheckBox chkBold;
+    public RadioButton rdMale;
+    public RadioButton rdFemale;
+    public Button btnClearSelection;
+    public Button btnGetGender;
+    public Button btnSetMale;
+    public Button btnSetFemale;
+    public ToggleGroup tglGender;
 
-    public void initialize(){
+    public void btnClearSelectionOnAction(ActionEvent actionEvent) {
+        tglGender.selectToggle(null);
+    }
+
+    public void btnGetGenderOnAction(ActionEvent actionEvent) {
+        System.out.println(tglGender.getSelectedToggle());
+    }
+
+    public void btnSetMaleOnAction(ActionEvent actionEvent) {
+        //rdMale.setSelected(true);
+        tglGender.selectToggle(rdMale);
+    }
+
+    public void btnSetFemaleOnAction(ActionEvent actionEvent) {
+        tglGender.selectToggle(rdFemale);
+    }
+
+    public void chkBoldOnAction(ActionEvent actionEvent) {
+        updatePreviewText();
+    }
+
+    public void chkItalicOnAction(ActionEvent actionEvent) {
+        updatePreviewText();
+    }
+
+    private void updatePreviewText() {
+        lblPreviewText.setStyle("%s;%s".formatted(
+                chkBold.isSelected() ? "-fx-font-weight: bold" : "-fx-font-weight: normal",
+                chkItalic.isSelected()? "-fx-font-style: italic": "-fx-font-style: normal"));
+    }
+
+    public void tglBoldOnAction(ActionEvent actionEvent) {
+        updatePreview();
+    }
+
+    public void tglItalicOnAction(ActionEvent actionEvent) {
+        updatePreview();
+    }
+
+    private void updatePreview() {
+        lblPreview.setStyle("%s;%s".formatted(tglBold.isSelected() ? "-fx-font-weight: bold" : "-fx-font-weight: normal",
+                tglItalic.isSelected()? "-fx-font-style: italic": "-fx-font-style: normal"));
+    }
+
+    public void initialize() {
         prgBar.progressProperty().bind(sldrProgress.valueProperty());
         prgBar.progressProperty().addListener((observable, oldValue, newValue) -> {
             lblProgress.setText("%.2f%%".formatted(newValue.doubleValue() * 100));
@@ -34,14 +94,20 @@ public class MainSceneController {
 
         txtPlainPassword.textProperty().bindBidirectional(txtPassword.textProperty());
         txtPlainPassword.setVisible(false);
-        btnShowHide.setTooltip(new Tooltip(txtPlainPassword.isVisible() ? "Hide password": "Show Password"));
+        btnShowHide.setTooltip(new Tooltip(txtPlainPassword.isVisible() ? "Hide password" : "Show Password"));
+        //dtPicker.setValue(LocalDate.now());
+        dtPicker.setValue(LocalDate.of(2000, 5, 3));
+    }
+
+    public void dtPickerOnAction(ActionEvent actionEvent) {
+        System.out.println(dtPicker.getValue());
     }
 
     public void btnShowHideOnAction(ActionEvent actionEvent) {
         txtPlainPassword.setVisible(!txtPlainPassword.isVisible());
         txtPassword.setVisible(!txtPassword.isVisible());
-        ((ImageView)btnShowHide.getGraphic()).setImage(new Image("/icon/%s".formatted(txtPlainPassword.isVisible() ? "show.png": "hide.png")));
-        btnShowHide.getTooltip().setText(txtPlainPassword.isVisible() ? "Hide password": "Show Password");
+        ((ImageView) btnShowHide.getGraphic()).setImage(new Image("/icon/%s".formatted(txtPlainPassword.isVisible() ? "show.png" : "hide.png")));
+        btnShowHide.getTooltip().setText(txtPlainPassword.isVisible() ? "Hide password" : "Show Password");
     }
 
     public void btnSelectOnAction(ActionEvent actionEvent) {

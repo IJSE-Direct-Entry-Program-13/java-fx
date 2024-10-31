0c28f4d6c9526ee63fd320ac05d88b7a3e38d169
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-10-31 13:04:35 +0530

    Implement the second list

--- 05-controls/fx-controls-6/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
+++ 05-controls/fx-controls-6/src/main/java/lk/ijse/dep13/fx/controller/MainSceneController.java
@@ -11,13 +11,23 @@ public class MainSceneController {
     public TextField txt1;
     public Button btnAdd1;
     public Button btnDelete1;
+    public TextField txt2;
+    public Button btnAdd2;
+    public ListView<String> lst2;
+    public Button btnDelete2;
 
     public void initialize(){
         btnDelete1.setDisable(true);
+        btnDelete2.setDisable(true);
         lst1.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
             // btnDelete1.setDisable(newValue.intValue() == -1);
             // btnDelete1.setDisable(lst1.getSelectionModel().getSelectedIndex() == -1);
             btnDelete1.setDisable(lst1.getSelectionModel().isEmpty());
+        });
+        lst2.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+            // btnDelete1.setDisable(newValue.intValue() == -1);
+            // btnDelete1.setDisable(lst1.getSelectionModel().getSelectedIndex() == -1);
+            btnDelete2.setDisable(lst2.getSelectionModel().isEmpty());
         });
     }
 
@@ -35,5 +45,27 @@ public class MainSceneController {
         lst1.getSelectionModel().clearSelection();
 //        String selectedItem = lst1.getSelectionModel().getSelectedItem();
 //        items.remove(selectedItem);
+    }
+
+    public void btnDelete2OnAction(ActionEvent actionEvent) {
+        ObservableList<String> items = lst2.getItems();
+        int selectedItemIndex = lst2.getSelectionModel().getSelectedIndex();
+        items.remove(selectedItemIndex);
+        lst2.getSelectionModel().clearSelection();
+    }
+
+    public void btnAdd2OnAction(ActionEvent actionEvent) {
+        ObservableList<String> items = lst2.getItems();
+        items.add(txt2.getText().strip());
+        txt2.clear();
+        txt2.requestFocus();
+    }
+
+    public void txt2OnAction(ActionEvent actionEvent) {
+        btnAdd2.fire();
+    }
+
+    public void txt1OnAction(ActionEvent actionEvent) {
+        btnAdd1.fire();
     }
 }
